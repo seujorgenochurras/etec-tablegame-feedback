91371b6704e6e323c6ea2180273363dba95bfc8b
seujorgenochurras <eusoulegaldemaiseverdadeeujuro@gmail.com>
2023-10-24 01:58:42 -0300

    I JUST WANNA SLEEP AAAAA

--- src/1a/votar/votar.js
+++ src/1a/votar/votar.js
@@ -96,7 +96,7 @@ function sendVoteRequest() {
         return __generator(this, function (_a) {
             switch (_a.label) {
                 case 0:
-                    url = "https://381f-2804-14d-8487-8cb6-2e9-ee2c-2ae8-323e/vote";
+                    url = "https://381f-2804-14d-8487-8cb6-2e9-ee2c-2ae8-323e.ngrok-free.app/vote";
 
                     feedback = {
                         tablegame: tablegameName,
